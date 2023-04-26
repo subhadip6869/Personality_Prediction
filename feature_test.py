871bb6c5310c33eac361c01a702ba77eb73005e6
Subhadip Dutta <subhadipdutta3000@gmail.com>
2023-04-26 09:31:01 +0530

    modified feature test for taking user input path

--- feature_test.py
+++ feature_test.py
@@ -1,7 +1,8 @@
 from package.features import get_letter_slant, get_line_slant, auto_crop_image
 import matplotlib.pyplot as plt
+import os
 
-image = r"Personality_Prediction\dataset\data1\training_set\Agreeableness\IMG_20200215_164151.jpg"
+image = os.path.normpath(input("Enter path: "))
 print(f"Letter slant: {get_letter_slant(image)}")
 print(f"Line slant: {get_line_slant(image)[0]}")
 
