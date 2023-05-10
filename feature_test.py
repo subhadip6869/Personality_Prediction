59320e8a7f6d28f19371d9268576271a15f172a1
anima saha <sahagolo2019@gmail.com>
2023-05-10 20:08:52 +0530

    feature_test.py updated

--- feature_test.py
+++ feature_test.py
@@ -1,10 +1,16 @@
 from package.features import *
 import matplotlib.pyplot as plt
+import numpy as np
 import os
 
 image = os.path.normpath(input("Enter path: "))
 print(f"Letter slant: {get_letter_slant(image)}")
 print(f"Line slant: {get_line_slant(image)[0]}")
+print(f"Letter size: {get_letter_size(image)[0]}")
+print(f"Word spacing: {gap_between_words(image)[0]}")
 
-plt.imshow(get_line_slant(image)[1])
+new_img = np.vstack([get_line_slant(image)[1], 
+                     get_letter_size(image)[1],
+                     gap_between_words(image)[1]])
+plt.imshow(new_img)
 plt.show()
