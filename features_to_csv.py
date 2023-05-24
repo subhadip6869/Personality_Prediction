42f214121edfaf318d680e4930ae82b307ab2235
Subhadip Dutta <subhadipdutta3000@gmail.com>
2023-05-24 13:20:23 +0530

    update

--- features_to_csv.py
+++ features_to_csv.py
@@ -38,6 +38,7 @@ for i in range(train_data.shape[0]):
     print("\rTrain data processed: {:3d}/{:3d}".format(i+1, train_data.shape[0]), end="")
     iminfo.append(get_letter_slant(image_path=train_data.image.tolist()[i]))
     iminfo.append(get_line_slant(image_path=train_data.image.tolist()[i])[0])
+    iminfo.append(get_margin_slope(image_path=train_data.image.tolist()[i]))
     iminfo.append(get_letter_size(image_path=train_data.image.tolist()[i])[0])
     iminfo.append(gap_between_words(image_path=train_data.image.tolist()[i])[0])
     x_train.append(iminfo)
@@ -50,6 +51,7 @@ for i in range(test_data.shape[0]):
     print("\rTest data processed: {:3d}/{:3d}".format(i+1, test_data.shape[0]), end="")
     iminfo.append(get_letter_slant(image_path=test_data.image.tolist()[i]))
     iminfo.append(get_line_slant(image_path=test_data.image.tolist()[i])[0])
+    iminfo.append(get_margin_slope(image_path=test_data.image.tolist()[i]))
     iminfo.append(get_letter_size(image_path=test_data.image.tolist()[i])[0])
     iminfo.append(gap_between_words(image_path=test_data.image.tolist()[i])[0])
     x_test.append(iminfo)
@@ -60,6 +62,7 @@ print()
 # converting train features and test labels into dictionaries
 traindataset = {'letter_slant': [],
                 'line_slant': [],
+                'margin_slope': [],
                 'letter_size': [],
                 'word_spacing': [],
                 'personality': []}
@@ -68,15 +71,17 @@ testdataset = copy.deepcopy(traindataset)
 for i in range(len(x_train)):
     traindataset['letter_slant'].append(x_train[i][0])
     traindataset['line_slant'].append(x_train[i][1])
-    traindataset['letter_size'].append(x_train[i][2])
-    traindataset['word_spacing'].append(x_train[i][3])
+    traindataset['margin_slope'].append(x_train[i][2])
+    traindataset['letter_size'].append(x_train[i][3])
+    traindataset['word_spacing'].append(x_train[i][4])
     traindataset['personality'].append(y_train[i][0])
 
 for i in range(len(x_test)):
     testdataset['letter_slant'].append(x_test[i][0])
     testdataset['line_slant'].append(x_test[i][1])
-    testdataset['letter_size'].append(x_test[i][2])
-    testdataset['word_spacing'].append(x_test[i][3])
+    testdataset['margin_slope'].append(x_test[i][2])
+    testdataset['letter_size'].append(x_test[i][3])
+    testdataset['word_spacing'].append(x_test[i][4])
     testdataset['personality'].append(y_test[i][0])
 
 # converting features into dataframe
